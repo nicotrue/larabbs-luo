61e53ef7a594430867fe07f2a9ea27f8dccf7bf0
罗城 <379891225@qq.com>
2019-07-23 15:30:43 +0800

    侧边栏链接

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -24,3 +24,17 @@
         </div>
     </div>
 @endif
+
+@if(count($links))
+    <div class="card mt-4">
+        <div class="card-body pt-2">
+            <div class="text-center mt-1 mb-0 text-muted">资源推荐</div>
+            <hr class="mt-2 mb-3">
+            @foreach($links as $link)
+                <a class="media mt-1" href="{{ $link->link() }}">
+                    <span class="media-heading text-muted">{{ $link->title }}</span>
+                </a>
+            @endforeach
+        </div>
+    </div>
+@endif
