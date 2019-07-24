264b24c8d6a8656f245d48558736976db6095e20
罗城 <379891225@qq.com>
2019-07-24 11:58:36 +0800

    install dingo and 修改view对象为数组

--- config/administrator/replies.php
+++ config/administrator/replies.php
@@ -24,7 +24,7 @@ return [
             'sortable' => false,
             'output'   => function ($value, $model) {
                 $avatar = $model->user->avatar;
-                $value = empty($avatar) ? 'N/A' : '<img src="'.$avatar.'" style="height:22px;width:22px"> ' . $model->user->name;
+                $value = empty($avatar) ? 'N/A' : '<img src="'.$avatar.'" style="height:22px;width:22px"> ' . $model->user['name'];
                 return model_link($value, $model->user);
             },
         ],
