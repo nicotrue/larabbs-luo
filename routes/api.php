770abe0250b89052012e85ffbc7e835a69720c95
罗城 <379891225@qq.com>
2019-08-01 10:18:50 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+$api->group([
+    'middleware' => 'api.throttle',
+    'limit' => config('api.rate_limits.sign.limit'),
+    'expires' => config('api.rate_limits.sign.expires'),
 ], function ($api) {
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
