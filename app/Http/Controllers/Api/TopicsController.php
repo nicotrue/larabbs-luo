2ff226a63ca3e5f04ad7ed351aef5bff73081a46
罗城 <379891225@qq.com>
2019-08-05 17:14:29 +0800

    'topicindex'

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,9 +6,32 @@ use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
 use App\Transformers\TopicTransformer;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
+    public function index(Request $request,Topic $topic)
+    {
+        $query = $topic->query();
+
+        if ($categoryId = $request->category_id){
+            $query->where('category_id',$categoryId);
+        }
+
+        switch ($request->order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics,new TopicTransformer());
+    }
+
     public function store(TopicRequest $request,Topic $topic)
     {
         $topic->fill($request->all());
@@ -32,5 +55,11 @@ class TopicsController extends Controller
 
         $topic->delete();
         return $this->response->noContent();
+    }
+
+    public function userIndex(User $user,Request $request)
+    {
+        $topics = $user->topics()->recent()->paginate(20);
+        return $this->response->paginator($topics,new TopicTransformer());
     }
 }
