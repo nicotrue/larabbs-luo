86ae24d992a38031f0cb8c61699c1b8df5c53952
罗城 <379891225@qq.com>
2019-08-05 15:06:28 +0800

    'topicsanc'

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware'=>'serializer:array'
+    'middleware'=>['serializer:array','bindings']
 ], function($api) {
 
     $api->group([
@@ -32,6 +32,12 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics','TopicsController@store')
                 ->name('api.topics.store');
+            //修改话题
+            $api->patch('topics/{topic}','TopicsController@update')
+                ->name('api.topics.update');
+            //删除话题
+            $api->delete('topics/{topic}','TopicsController@destroy')
+                ->name('api.topics.destroy');
         });
 
         // 短信验证码
