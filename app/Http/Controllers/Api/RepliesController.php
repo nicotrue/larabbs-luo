55bcf422816896e420d9c110ff65d4f8b082f4c9
罗城 <379891225@qq.com>
2019-08-06 15:47:36 +0800

    'repliesstore'

---
+++ app/Http/Controllers/Api/RepliesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\ReplyRequest;
+use App\Models\Reply;
+use App\Models\Topic;
+use App\Transformers\ReplyTransformer;
+
+class RepliesController extends Controller
+{
+    public function store(Reply $reply,Topic $topic,ReplyRequest $request)
+    {
+        $reply->content = $request->content;
+        $reply->topic()->associate($topic);
+        $reply->user()->associate($this->user());
+        $reply->save();
+
+        return $this->response->item($reply,new ReplyTransformer())->setStatusCode(201);
+    }
+}
