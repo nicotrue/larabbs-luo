3fe42bf0ca5a5567f0235d3ee1778e6f8830d9b3
罗城 <379891225@qq.com>
2019-08-07 12:56:53 +0800

    'pushnotification'

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         App\Providers\EasySmsServiceProvider::class,
+        App\Providers\JpushServiceProvider::class,
 
     ],
 
