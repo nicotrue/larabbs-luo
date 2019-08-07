726ddf840d51667cb7d156790bc802cfc8c976db
罗城 <379891225@qq.com>
2019-08-07 11:21:40 +0800

    'activedusers'

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,9 @@ $api->version('v1', [
         // 资源推荐
         $api->get('links','linksController@index')
             ->name('api.links.index');
+        // 活跃用户
+        $api->get('actived/users','UsersController@activedIndex')
+            ->name('api.actived.user.index');
 
         //需要 token 验证的接口
         $api->group(['middleware'=>'api.auth'],function ($api){
