ce58e4979811fe381ae16b0f192bae5282c281b2
罗城 <379891225@qq.com>
2019-08-23 15:15:12 +0800

    'weapplogin'

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,7 +36,7 @@ class User extends Authenticatable implements MustVerifyEmailContract, JWTSubjec
     }
 
     protected $fillable = [
-        'name', 'phone','email', 'password','introduction','avatar','weixin_openid', 'weixin_unionid','registration_id'
+        'name', 'phone','email', 'password','introduction','avatar','weixin_openid', 'weixin_unionid','registration_id','weixin_session_key', 'weapp_openid',
     ];
 
     protected $hidden = [
